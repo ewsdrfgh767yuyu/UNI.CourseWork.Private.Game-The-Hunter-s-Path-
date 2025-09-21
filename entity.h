7a7c7a8d0f14b1376d52de50abe2a57b0f1bd5da
Stanislau Mikulski <stasmik1209@gmail.com>
2025-09-21 23:01:35 +0300

    изменен класс игрока под класс предметов

--- entity.h
+++ entity.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <string>
 #include <map>
+#include <vector>
 using namespace std;
 
 enum class ItemType {
@@ -24,7 +25,7 @@ enum class EquipmentSlot {
 	NECK,
 	RING1,
 	RING2,
-	NONE // Для неподходящих предметов
+	NONE 
 };
 
 class Item {
@@ -33,7 +34,7 @@ private:
 	string m_description;
 	ItemType m_type;
 	EquipmentSlot m_slot;
-	map<string, int> m_stats; // Характеристики предмета
+	map<string, int> m_stats;
 
 public:
 	Item(const string& name, const  string& description,
@@ -42,7 +43,6 @@ public:
 		: m_name(name), m_description(description),
 		m_type(type), m_slot(slot), m_stats(stats) {}
 
-	// Геттеры
 	string getName() const { return m_name; }
 	string getDescription() const { return m_description; }
 	ItemType getType() const { return m_type; }
@@ -52,7 +52,6 @@ public:
 		return it != m_stats.end() ? it->second : 0;
 	}
 
-	// Отображение информации о предмете
 	void displayInfo() const {
 		cout << "=== " << m_name << " ===\n";
 		cout << "Type: ";
@@ -224,46 +223,193 @@ private:
 	int m_level;
 	int m_required_experience;
 	int m_received_experience;
+
+	vector<Item> m_inventory;
+	map<EquipmentSlot, Item> m_equipment;
+
 	void increaseRequiredExperience() {
 		int required_experience = 250 * pow((m_level + 1), 1.8);
 		setRequiredExperience(required_experience);
 	}
+
+	void recalculateStats() {
+		int healthBonus = 0;
+		int damageBonus = 0;
+		int defenseBonus = 0;
+		int attackBonus = 0;
+		int staminaBonus = 0;
+		int initiativeBonus = 0;
+
+		for (const auto& item : m_equipment) {
+			healthBonus += item.second.getStat("health");
+			damageBonus += item.second.getStat("damage");
+			defenseBonus += item.second.getStat("defense");
+			attackBonus += item.second.getStat("attack");
+			staminaBonus += item.second.getStat("stamina");
+			initiativeBonus += item.second.getStat("initiative");
+		}
+
+		Entity::setMaxHealthPoint(100 + 10 * (m_level - 1) + healthBonus);
+		Entity::setDamage(10 + 2 * (m_level - 1) + damageBonus);
+		Entity::setDefence(0 + 1 * (m_level - 1) + defenseBonus);
+		Entity::setAttack(0 + 1 * (m_level - 1) + attackBonus);
+		Entity::setMaxStamina(1 + (m_level / 5) + staminaBonus);
+		Entity::setInitiative(10 + (m_level / 3) + initiativeBonus);
+
+		regenerateStamina();
+	}
+
+	static const map<EquipmentSlot, string> slotNames;
+
 public:
+
 	Player(int max_hp = 100, int damage = 10, int defense = 0,
 		int attack = 0, int max_stamina = 1, int c_stamina = 1, int initiative = 10,
-		int attack_range = 0, int level = 1, int required_experience = 1000, int received_experience = 0)
+		int attack_range = 0, int level = 1, int required_experience = 1000,
+		int received_experience = 0)
 		: Entity(max_hp, damage, defense, attack,
 			max_stamina, c_stamina, initiative, attack_range),
-		m_level(level), m_required_experience(required_experience), m_received_experience(received_experience) {}
+		m_level(level), m_required_experience(required_experience),
+		m_received_experience(received_experience) {
+
+		m_equipment = {
+			{EquipmentSlot::HEAD, Item("None", "No item", ItemType::ARMOR, EquipmentSlot::HEAD, {})},
+			{EquipmentSlot::CHEST, Item("None", "No item", ItemType::ARMOR, EquipmentSlot::CHEST, {})},
+			{EquipmentSlot::HANDS, Item("None", "No item", ItemType::ARMOR, EquipmentSlot::HANDS, {})},
+			{EquipmentSlot::LEGS, Item("None", "No item", ItemType::ARMOR, EquipmentSlot::LEGS, {})},
+			{EquipmentSlot::FEET, Item("None", "No item", ItemType::ARMOR, EquipmentSlot::FEET, {})},
+			{EquipmentSlot::MAIN_HAND, Item("None", "No item", ItemType::WEAPON, EquipmentSlot::MAIN_HAND, {})},
+			{EquipmentSlot::OFF_HAND, Item("None", "No item", ItemType::WEAPON, EquipmentSlot::OFF_HAND, {})},
+			{EquipmentSlot::NECK, Item("None", "No item", ItemType::ACCESSORY, EquipmentSlot::NECK, {})},
+			{EquipmentSlot::RING1, Item("None", "No item", ItemType::ACCESSORY, EquipmentSlot::RING1, {})},
+			{EquipmentSlot::RING2, Item("None", "No item", ItemType::ACCESSORY, EquipmentSlot::RING2, {})}
+		};
+	}
 
 	void setLevel(int level) {
 		if (level > 0) {
 			m_level = level;
 		}
 		else {
-			throw  invalid_argument("Level cannot be negative!");
+			throw invalid_argument("Level cannot be negative!");
 		}
 	}
+
 	void setRequiredExperience(int req_level) {
 		if (req_level > 0) {
 			m_required_experience = req_level;
 		}
 		else {
-			throw  invalid_argument("Experience cannot be negative!");
+			throw invalid_argument("Experience cannot be negative!");
 		}
 	}
+
 	void setReceivedExperience(int rec_level) {
 		if (rec_level > 0) {
 			m_received_experience = rec_level;
 		}
 		else {
-			throw  invalid_argument("Experience cannot be negative!");
+			throw invalid_argument("Experience cannot be negative!");
 		}
 	}
 
-	int getLevel() const { return m_level; };
-	int getRequiredExperience() const { return m_required_experience; };
-	int getReceivedExperience() const { return m_received_experience; };
+	int getLevel() const { return m_level; }
+	int getRequiredExperience() const { return m_required_experience; }
+	int getReceivedExperience() const { return m_received_experience; }
+
+	void addItem(const Item& item) {
+		m_inventory.push_back(item);
+	}
+
+	void equipItem(int inventoryIndex) {
+		if (inventoryIndex < 0 || inventoryIndex >= m_inventory.size()) {
+			throw out_of_range("Invalid inventory index");
+		}
+
+		Item item = m_inventory[inventoryIndex];
+		EquipmentSlot slot = item.getSlot();
+
+		if (slot == EquipmentSlot::NONE) {
+			cout << "This item cannot be equipped.\n";
+			return;
+		}
+
+		if (m_equipment[slot].getName() != "None") {
+			unequipItem(slot);
+		}
+
+		m_equipment[slot] = item;
+		m_inventory.erase(m_inventory.begin() + inventoryIndex);
+
+		recalculateStats();
+
+		cout << "Equipped: " << item.getName() << "\n";
+	}
+
+	void unequipItem(EquipmentSlot slot) {
+		if (m_equipment[slot].getName() == "None") {
+			cout << "No item equipped in this slot.\n";
+			return;
+		}
+
+		m_inventory.push_back(m_equipment[slot]);
+
+		m_equipment[slot] = Item("None", "No item",
+			slot == EquipmentSlot::MAIN_HAND || slot == EquipmentSlot::OFF_HAND ?
+			ItemType::WEAPON : ItemType::ARMOR,
+			slot, {});
+
+		recalculateStats();
+
+		cout << "Unequipped item from slot.\n";
+	}
+
+	void displayInventory() const {
+		cout << "=== INVENTORY ===\n";
+		if (m_inventory.empty()) {
+			cout << "Inventory is empty.\n";
+		}
+		else {
+			for (size_t i = 0; i < m_inventory.size(); ++i) {
+				cout << i << ": " << m_inventory[i].getName() << "\n";
+			}
+		}
+		cout << "=================\n";
+	}
+
+	void displayEquipment() const {
+		cout << "=== EQUIPMENT ===\n";
+		for (const auto& slot : m_equipment) {
+			cout << slotNames.at(slot.first) << ": " << slot.second.getName() << "\n";
+		}
+		cout << "=================\n";
+	}
+
+	void useConsumable(int inventoryIndex) {
+		if (inventoryIndex < 0 || inventoryIndex >= m_inventory.size()) {
+			throw out_of_range("Invalid inventory index");
+		}
+
+		Item item = m_inventory[inventoryIndex];
+		if (item.getType() != ItemType::CONSUMABLE) {
+			cout << "This item is not consumable.\n";
+			return;
+		}
+
+		int healthRestore = item.getStat("health_restore");
+		if (healthRestore > 0) {
+			heal(healthRestore);
+		}
+
+		int staminaRestore = item.getStat("stamina_restore");
+		if (staminaRestore > 0) {
+			setCurrentStamina(min(getMaxStamina(), getCurrentStamina() + staminaRestore));
+		}
+
+		m_inventory.erase(m_inventory.begin() + inventoryIndex);
+
+		cout << "Used: " << item.getName() << "\n";
+	}
 
 	void upLevel() {
 		while (m_received_experience >= m_required_experience) {
@@ -271,16 +417,22 @@ public:
 			setLevel(m_level + 1);
 			increaseRequiredExperience();
 
-			setMaxHealthPoint(static_cast<int>(getMaxHealthPoint() * 1.1));
-			setDamage(static_cast<int>(getDamage() + 2));
-			setDefence(static_cast<int>(getDefense() + 1));
-			setAttack(static_cast<int>(getAttack() + 1));
-			setInitiative(static_cast<int>(getInitiative() ));
-
-			setCurrentHealthPoint(getMaxHealthPoint());
-			setCurrentStamina(getMaxStamina());
+			recalculateStats();
 		}
 	}
+};
+
+const map<EquipmentSlot, string> Player::slotNames = {
+	{EquipmentSlot::HEAD, "Head"},
+	{EquipmentSlot::CHEST, "Chest"},
+	{EquipmentSlot::HANDS, "Hands"},
+	{EquipmentSlot::LEGS, "Legs"},
+	{EquipmentSlot::FEET, "Feet"},
+	{EquipmentSlot::MAIN_HAND, "Main Hand"},
+	{EquipmentSlot::OFF_HAND, "Off Hand"},
+	{EquipmentSlot::NECK, "Neck"},
+	{EquipmentSlot::RING1, "Ring 1"},
+	{EquipmentSlot::RING2, "Ring 2"}
 };
 
 class Enemy : public Entity {
